cdc0e61986cfed11f28a11807033a9341a7cdce2
cheng2qu <chengcheng.qu@gmail.com>
2019-08-21 22:41:27 +0200

    Delect old code

--- build/code/data_lag.R
+++ build/code/data_lag.R
@@ -30,7 +30,27 @@ fs$Cash_c[which(is.infinite(fs$Cash_c))] <- NA
 lag Income
 lag insurance
 
+# Lag of quake event
+fs <- fs[, n_1 := shift(Neighbor*Mag, n=1L, type = "lead"), by = Stkcd]
+fs <- fs[, s_1 := shift(Struck*Mag, n=1L, type = "lead"), by = Stkcd]
 
+fs <- fs[, n1 := shift(Neighbor*Mag, n=1L, type = "lag"), by = Stkcd]
+fs <- fs[, s1 := shift(Struck*Mag, n=1L, type = "lag"), by = Stkcd]
+fs <- fs[, n2 := shift(Neighbor*Mag, n=2L, type = "lag"), by = Stkcd]
+fs <- fs[, s2 := shift(Struck*Mag, n=2L, type = "lag"), by = Stkcd]
+fs <- fs[, n3 := shift(Neighbor*Mag, n=3L, type = "lag"), by = Stkcd]
+fs <- fs[, s3 := shift(Struck*Mag, n=3L, type = "lag"), by = Stkcd]
+fs <- fs[, n4 := shift(Neighbor*Mag, n=4L, type = "lag"), by = Stkcd]
+fs <- fs[, s4 := shift(Struck*Mag, n=4L, type = "lag"), by = Stkcd]
+
+fs <- fs[, n5 := shift(Neighbor*Mag, n=5L, type = "lag"), by = Stkcd]
+fs <- fs[, s5 := shift(Struck*Mag, n=5L, type = "lag"), by = Stkcd]
+fs <- fs[, n6 := shift(Neighbor*Mag, n=6L, type = "lag"), by = Stkcd]
+fs <- fs[, s6 := shift(Struck*Mag, n=6L, type = "lag"), by = Stkcd]
+fs <- fs[, n7 := shift(Neighbor*Mag, n=7L, type = "lag"), by = Stkcd]
+fs <- fs[, s7 := shift(Struck*Mag, n=7L, type = "lag"), by = Stkcd]
+fs <- fs[, n8 := shift(Neighbor*Mag, n=8L, type = "lag"), by = Stkcd]
+fs <- fs[, s8 := shift(Struck*Mag, n=8L, type = "lag"), by = Stkcd]
 
 ## Lead fs data
 # One quarter forward on cash ratio
