0484dd26649bedaf4990723f02d78ed8b5e8feba
cheng2qu <chengcheng.qu@gmail.com>
2019-08-21 15:18:17 +0200

    Add treatment function

--- build/code/scratch_paper.R
+++ build/code/scratch_paper.R
@@ -10,3 +10,45 @@ earthquake[, c("Date","Lat","Lon") :=
 # E.G, n=3, and return group index
 x <- 2:18
 as.numeric(cut(x,breaks = 3))
+
+# Function passing list to argument
+tttest <- function(date, earthquake){
+  event <- earthquake[Date == date,]
+  return(event)
+}
+
+# Unit test works
+tttest("20070602", earthquake)
+
+# Great-circle distance
+GreatCircleDist <- function(LatA, LonA, LatB, LonB, R){
+  # Formula for great-circle distance
+  # With point A and B's longitude and latitude LatA, LonA, LatB, LonB
+  # The central angle: arccos(sin LatA sin LatB + cos LatA cos LatB cos |LonA - lonB|)
+  # The GC distance: R * central angle
+  
+  # Set the radius as mean earth radius
+  if (missing(R)) {
+    R = 6371
+  }
+  
+  # Check if lon and lat are measured in pi
+  if (any(abs(c(LatA, LonA, LatB, LonB)) > pi)){
+    warning("Coordinate beyond range 2*Pi")
+  }
+  
+  # Calculate central angle C
+  C=sin(LatA)*sin(LatB) + cos(LatA)*cos(LatB)*cos(abs(LonA-LonB))
+  
+  # Calculate GC dist
+  dist = R*acos(C)
+  return(dist)
+}
+
+# Unit test GreatCircleDist()
+GreatCircleDist(earthquake$Lat[1],earthquake$Lon[1],earthquake$Lat[2],earthquake$Lon[2])
+
+sapply(1:nrow(earthquake), function(x){
+  GreatCircleDist(0.3, 0.1, earthquake$Lat[x],earthquake$Lon[x])
+})
+
