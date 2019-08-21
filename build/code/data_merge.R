35629796c707343dfa64ba6a829cb095c07bc2a4
cheng2qu <chengcheng.qu@gmail.com>
2019-08-21 05:22:23 +0200

    Merge fs data, remove replicated tables, save merged data

--- build/code/data_merge.R
+++ build/code/data_merge.R
@@ -1,70 +1,55 @@
-## Prepare financial statement data
-## Require packages: data.table, zoo
-## Require data tables: company, CPI, earthquake
+## Merge data tables with fs
+## Calculate variables from merged data
 
-## Load data and filter sample -----
+## Setup -----
+# Set the working directory to access temp data
+temp_dir <- "~/R project/Code replication/build/temp"
+setwd(temp_dir)
 
-fs <- fread("Cash_RD.csv")
+# Set link to temp data
+source_data <- "data_pre_merge.RData"
+# Load filter data
+load(source_data)
 
-names(fs) <- c("Stkcd","Accper","Reptyp","Cash","RD","TA","DebtL","TL","TE")
+## Merge tables -----
+# Merge fs with industry code
+fs <- merge(fs,ind,by = "Stkcd", all.x = TRUE)
+# Merge fs with company location
+fs <- merge(fs,company,by = "Stkcd", all.x = TRUE)
+# Merge fs with Income
+fs <- merge(fs,Income,by = c("Stkcd","Accper"), all.x = TRUE)
+# Merge fs with Insurance
+fs <- merge(fs,Insurance,by = c("Stkcd","Accper"), all.x = TRUE)
+# Merge fs with CPI
+fs <- merge(fs,CPI,by.x = "Accper", by.y ="Season", all.x = TRUE)
 
-# Keep only observations with consolidated statement
-fs <- fs[fs$Reptyp=="A",] 
-
-# Keep only observations in the list of company sample
-fs <- fs[fs$Stkcd %in% company$Stkcd,] 
-
-# Drop year beginning observations
-fs <- fs[!grepl("/1/1",fs$Accper),] 
-
-# Substract season(quarter)
-fs$Quarter <- as.yearqtr(fs$Accper, format = "%Y/%m/%d")
-
-# Substract subsample of MSE shares
-fs_midsm <- fs[fs$Stkcd>002000&fs$Stkcd<002999,] 
-
-# Keep only main board stock market
-fs <- fs[fs$Stkcd<002000|fs$Stkcd>002999,]
-
-## Should here starts another script?
-
-## Merge with industry index -----
-
-# Merge by key: Stkcd
-fs <-merge(fs,ind,by = "Stkcd", all = FALSE)
-
-# Create cross index
+## Create cross index -----
 fs$Ind_Season <- paste(fs$Indcd,fs$Season %% 10000,sep = "_") # Industry-season index
-
-## Merge with city location -----
-fs$City <- company$Regplc[match(fs$Stkcd, company$Stkcd)] # Adding location factor into financial data
 fs$City_Season <- paste(fs$City,fs$Season %% 10000,sep = "_") # City-season index
 
-## Adjust to inflation -----
+## Adjust fs with CPI -----
+fs[, c("Cash","RD","TA","TL","TE","DebtL","OpIncome","InsuranceExp") :=
+  lapply(.SD, "/", CPI), .SDcols = c("Cash","RD","TA","TL","TE","DebtL","OpIncome","InsuranceExp")]
 
-# Merge with CPI
-
-# Divide by acc. CPI for all ab. column at once?
-
-fs$Cash <- fs$Cash/CPI$CPI[match(fs$Accper,CPI$Season)]
-fs$RD <- fs$RD/CPI$CPI[match(fs$Accper,CPI$Season)]
-fs$TA <- fs$TA/CPI$CPI[match(fs$Accper,CPI$Season)]
-fs$TL <- fs$TL/CPI$CPI[match(fs$Accper,CPI$Season)]
-fs$TE <- fs$TE/CPI$CPI[match(fs$Accper,CPI$Season)]
-fs$DebtL <- fs$DebtL/CPI$CPI[match(fs$Accper,CPI$Season)]
-
-## Calculate the ratios -----
-
-fs$TA[fs$TA==0] <- NA # Mark records where total asset is zero
-fs$Cash_p <- fs$Cash/fs$TA # Calculate cash holding ratio
-fs$Leverage <- log(fs$TL) - log(fs$TA-fs$TL) # Calculate leverage ratio
-fs$Leverage[which(is.infinite(fs$Leverage)&fs$Leverage>0)] <- 9999 # Give value to infinte leverage ratio, i.e. TA=TL
-fs$Leverage[which(is.infinite(fs$Leverage)&fs$Leverage<0)] <- -9999
+## Calculate ratios -----
+# Cash holding ratio
+fs[, Cash_p := Cash/TA]
+# Leverage ratio
+fs[, Leverage := TL/TE]
+# Asign value to infinte leverage ratio (when TA=TL)
+fs$Leverage[which(fs$Leverage>=1000)] <- 1000 
+fs$Leverage[which(fs$Leverage<=-1000)] <- -1000
 
 ## Mark the sample into thirds by measures
-fs <- fs[, c("quanTA","quanOI","quanLev", "quanCash_p") := 
-           list(findInterval(TA, quantile(TA,na.rm = TRUE, probs = c(0,1/3,2/3))),#c(0,0.1, 0.3, 0.5,0.7, 0.9))),
-                findInterval(OpIncome, quantile(OpIncome,na.rm = TRUE, probs = c(0,1/3,2/3))),#c(0,0.1, 0.3, 0.5,0.7, 0.9)))
-                findInterval(Leverage, quantile(Leverage,na.rm = TRUE, probs = c(0,1/3,2/3))),#c(0,0.1, 0.3, 0.5,0.7, 0.9)))
-                findInterval(Cash_p, quantile(Cash_p,na.rm = TRUE, probs = c(0,1/3,2/3)))#c(0,0.1, 0.3, 0.5,0.7, 0.9)))
-           ),   by = "Accper"]
+# Level for equal-size groups
+n <- 3
+fs[, c("quanTA","quanOI","quanLev", "quanCash_p") := 
+     lapply(.SD, cut, breaks = n),
+   .SDcol=c("TA","OpIncome","Leverage","Cash_p"),
+   by = "Accper"]
+
+## Save merged data
+# Drop merged tables
+rm(company,ind, CPI,Income,Insurance)
+# Save data to temp
+save.image("data_merged.RData")
