5c3d825fe4eb40399724a8929e72fafc3a9f131b
cheng2qu <chengcheng.qu@gmail.com>
2019-08-21 18:05:40 +0200

    Lag earthquake treatment variables

--- build/code/data_lag.R
+++ build/code/data_lag.R
@@ -1,6 +1,16 @@
-## Lag the data
+## Create lagged and lead term for fs
 
+## Setup -----
+# Set the working directory to access temp data
+temp_dir <- "~/R project/Code replication/build/temp"
+setwd(temp_dir)
 
+# Set link to temp data
+source_data <- "data_merged.RData"
+# Load filter data
+load(source_data)
+
+## Lag fs data
 fs <- fs[which(!is.na(fs$Cash_p)),] # Delete records when total asset is zero
 fs <- fs[which(!is.na(fs$Leverage)),] # Delete records when total equity is negative
 
@@ -16,5 +26,13 @@ fs <- fs[, Cash_c := log(Cash_p)-log(Cash_p_d), by = Stkcd] # Lag the cash holdi
 fs$Cash_c[which(is.nan(fs$Cash_c))] <- NA
 fs$Cash_c[which(is.infinite(fs$Cash_c))] <- NA
 
-# One quarter forward
+
+lag Income
+lag insurance
+
+
+
+## Lead fs data
+# One quarter forward on cash ratio
 fs <- fs[, Cash_p_1f := shift(Cash_p, n=1L, type = "lead"), by = Stkcd] # Lead the cash holding ratio
+
