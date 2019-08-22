f2961fc1379701d50f5897d18d66e95a6ff23834
cheng2qu <chengcheng.qu@gmail.com>
2019-08-22 03:46:00 +0200

    R script alternative to run dirctory in order

---
+++ runDirectory.R
@@ -0,0 +1,23 @@
+## Script to run all steps in order
+
+## Empty temp folders
+temp_dir <- c("~/R project/Code replication/build/temp/",
+              "~/R project/Code replication/analysis/temp/")
+temp <- lapply(temp_dir, function(x) 
+  file.remove(file.path(x), list.files(x)))
+
+## Run scripts in build part
+fileDir <- "~/R project/Code replication/build/code/"
+fileList <- c("data_filter.R",
+              "data_merge.R",
+              "data_treatment.R",
+              "data_split.R",
+              "data_lag.R")
+filePath <- paste0(fileDir,fileList)
+temp <- lapply(filePath, source)
+
+## Run scripts in analysis part
+fileDir <- "~/R project/Code replication/analysis/code/"
+fileList <- c()
+filePath <- paste0(fileDir,fileList)
+temp <- lapply(filePath, source)
