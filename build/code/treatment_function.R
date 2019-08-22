7dcee764a02203a16f7e0c81d522a216241dd7dc
cheng2qu <chengcheng.qu@gmail.com>
2019-08-22 22:03:16 +0200

    Rewrite StrikeTreatment Previous one mismatch dist to all fs obs at the same quarter Now only obs and quake under the same quarter has "finite" dist

--- build/code/treatment_function.R
+++ build/code/treatment_function.R
@@ -27,35 +27,38 @@ GreatCircleDist <- function(LatA, LonA, LatB, LonB, R){
 
 ## Function asigns treatment -----
 StrikeTreatment <- function(sampleData, quake, period){
-  
-  # Calculate the dist to strikes
-  # Asign treatment group by dist comparing with MMI5 and MMI1 dists
-  
   # Period can be either quarter or year
+  
+  # Set a number big enough as Inf
+  R <- 6000
+  
   # Refer lon and lat with pi
   quake[, c("Lat", "Lon") := lapply(.SD, "/", 180*pi), .SDcol = c("Lat", "Lon")]
   sampleData[, c("Lat", "Lon") := lapply(.SD, "/", 180*pi), .SDcol = c("Lat", "Lon")]
   
   # Initiate the treatment
   sampleData[, c("Struck", "Neighbor", "Depth", "Mag"):=0]
-  
+  # Calculate the dist to seimic center regardless of time
   distList <- sampleData[, lapply(1:nrow(quake), 
                                   function(x) GreatCircleDist(Lat, Lon, quake$Lat[x], quake$Lon[x]))]
+  # If accounting period has no earthquake, add R~6000 to dist
+  # so this data point would be given any treatment based on dist
+  distAdj <- sampleData[, lapply(1:nrow(quake), 
+                                 function(x) (get(period)!= quake[[period]][x])*R)]
+  distList <- distList + distAdj
+  
+  # Substract the closet distance
   sampleData[, dist:=apply(distList, 1, FUN=min)]
   sampleData[, minInd:=apply(distList, 1, FUN=which.min)]
   
   sampleData[, distMMI5:= quake$MMI5[minInd]]
   sampleData[, distMMI1:= quake$MMI1[minInd]]
   
-  validInd <- which(sampleData[[period]] %in%  quake[[period]])
-  if (length(validInd)>0){
-    
-    sampleData[validInd, Depth:= quake$Depth[minInd]]
-    sampleData[validInd, Mag:= quake$Mag[minInd]]
-    
-    sampleData[intersect(validInd, which(dist<=distMMI5)), Struck := 1]
-    sampleData[intersect(validInd, which(dist>distMMI5 & dist<=distMMI1)), Neighbor := 1]  
-  }
+  # Asign treatment group by dist comparing with MMI5 and MMI1 dists
+  sampleData[dist<=distMMI5, Struck := 1]
+  sampleData[dist>distMMI5 & dist<=distMMI1, Neighbor := 1] 
+  sampleData[dist<=distMMI1, Depth:= quake$Depth[minInd]]
+  sampleData[dist<=distMMI1, Mag:= quake$Mag[minInd]]
   
   return(sampleData[, c("Struck", "Neighbor", "Depth", "Mag")])
 }  
