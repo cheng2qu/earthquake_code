cd67af6fac3ff10110fc86081e6d72d189239fca
cheng2qu <chengcheng.qu@gmail.com>
2019-08-27 13:47:45 +0200

    Add analysis scripts to run directory

--- runDirectory.R
+++ runDirectory.R
@@ -5,6 +5,10 @@ temp_dir <- "~/R project/Code replication/build/temp/"
 setwd(temp_dir)
 file.remove(file.path(temp_dir), list.files(temp_dir))
 
+temp_dir <- "~/R project/Code replication/analysis/output/"
+setwd(temp_dir)
+file.remove(file.path(temp_dir), list.files(temp_dir))
+
 temp_dir <- "~/R project/Code replication/analysis/temp/"
 setwd(temp_dir)
 file.remove(file.path(temp_dir), list.files(temp_dir))
@@ -16,11 +20,16 @@ fileList <- c("data_filter.R",
               "data_treatment.R",
               "data_split.R",
               "data_lag.R")
-filePath <- paste0(fileDir,fileList)
+filePath <- paste0(fileDir, fileList)
 lapply(filePath, source)
 
 ## Run scripts in analysis part
 fileDir <- "~/R project/Code replication/analysis/code/"
-fileList <- c()
+fileList <- c("stats_summary.R",
+              "model1.R",
+              "model2.R",
+              "model3.R",
+              "model4.R",
+              "model5.R")
 filePath <- paste0(fileDir,fileList)
 lapply(filePath, source)
